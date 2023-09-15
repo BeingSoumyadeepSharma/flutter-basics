4fb80fe07d3fb7af3a630ad6bee1f834a6246211
Soumyadeep Sharma <soumyadeepsharma99@gmail.com>
2023-09-15 12:59:29 +0530

    Separating widgets in Flutter :hammer:

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:basics/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
@@ -10,29 +11,4 @@ void main() {
   );
 }
 
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 44, 20, 100),
-            Color.fromARGB(255, 42, 4, 81),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-          ),
-        ),
-      ),
-    );
-  }
-}
+
